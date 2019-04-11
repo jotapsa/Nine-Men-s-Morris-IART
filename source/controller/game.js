7298b93c21c0990adb259b0364b4f2f2f0eb4da5
João Sá <up201506252@fe.up.pt>
2019-04-11 23:47:46 +0000

    :construction:

--- source/controller/game.js
+++ source/controller/game.js
@@ -1,5 +1,5 @@
 class Game {
   constructor() {
-    this.board = [];
+
   }
 }
---
+++ source/model/game-model.js
@@ -0,0 +1,35 @@
+const Cell = Object.freeze({
+  BLOCKED: Symbol('blocked'),
+  OPEN: Symbol('open'),
+  RED: Symbol('red'),
+  BLUE: Symbol('blue'),
+});
+
+const Turn = Object.freeze({
+  RED: Symbol('red'),
+  BLUE: Symbol('blue'),
+});
+
+const GameState = Object.freeze({
+  PLACING: Symbol('placing'),
+  MOVING: Symbol('moving'),
+});
+
+class GameModel {
+  constructor() {
+    // eslint-disable-next-line max-len
+    this.board = [
+      [Cell.OPEN, Cell.BLOCKED, Cell.BLOCKED, Cell.OPEN, Cell.BLOCKED, Cell.BLOCKED, Cell.OPEN],
+      [Cell.BLOCKED, Cell.OPEN, Cell.BLOCKED, Cell.OPEN, Cell.BLOCKED, Cell.OPEN, Cell.BLOCKED],
+      [Cell.BLOCKED, Cell.BLOCKED, Cell.OPEN, Cell.OPEN, Cell.OPEN, Cell.BLOCKED, Cell.BLOCKED],
+      [Cell.OPEN, Cell.OPEN, Cell.OPEN, Cell.BLOCKED, Cell.OPEN, Cell.OPEN, Cell.OPEN],
+      [Cell.BLOCKED, Cell.BLOCKED, Cell.OPEN, Cell.OPEN, Cell.OPEN, Cell.BLOCKED, Cell.BLOCKED],
+      [Cell.BLOCKED, Cell.OPEN, Cell.BLOCKED, Cell.OPEN, Cell.BLOCKED, Cell.OPEN, Cell.BLOCKED],
+      [Cell.OPEN, Cell.BLOCKED, Cell.BLOCKED, Cell.OPEN, Cell.BLOCKED, Cell.BLOCKED, Cell.OPEN],
+    ];
+
+    this.turn = Turn.RED;
+
+    this.gameState = GameState.PLACING;
+  }
+}
