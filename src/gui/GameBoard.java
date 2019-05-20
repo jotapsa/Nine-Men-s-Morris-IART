d36f9d261d7489265d75b7abf6a7cb60ff324d3c
Duarte Carvalho <up201503661@fe.up.pt>
2019-05-20 12:41:45 +0100

    created gameboard class and added missing necessary components

---
+++ src/gui/GameBoard.java
@@ -0,0 +1,46 @@
+package gui;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class GameBoard extends JPanel implements MouseListener {
+	private JFrame parentComponent;
+	
+	public GameBoard(JFrame parent) {
+		
+		parentComponent = parent;
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+}
--- src/gui/NineMensMorrisGUI.java
+++ src/gui/NineMensMorrisGUI.java
@@ -1,14 +1,13 @@
 package gui;
 
-import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.FlowLayout;
-import java.awt.Frame;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -20,7 +19,8 @@ public class NineMensMorrisGUI extends JFrame {
 	private JPanel contentPane;
 	private JButton newGameButton;
 	private JButton aboutButton;
-	private JFrame aux;
+	private JComboBox gameModeSelector;
+	private GameBoard board;
 	
 
 	/**
@@ -43,8 +43,7 @@ public class NineMensMorrisGUI extends JFrame {
 	 * Create the frame.
 	 */
 	public NineMensMorrisGUI() {
-//		super("Nine Men's Morris");
-		aux = this;
+		super("Nine Men's Morris");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(555, 240, 720, 720);
 		contentPane = new JPanel();
@@ -52,15 +51,39 @@ public class NineMensMorrisGUI extends JFrame {
 		contentPane.setLayout(null);
 		setContentPane(contentPane);
 		
+		createNewGameButton();
 		
+		createAboutButton();
+		
+		createGameModeSelector();
+		
+		createGameBoard();
+	}
+	
+	private void createGameBoard() {
+		board = new GameBoard(this);
+		board.setBounds(12, 60, 680, 600);
+//		board.setBackground(new Color(128, 0, 128));
+		contentPane.add(board);
+	}
+
+	public void createGameModeSelector() {
+		JLabel lblGameMode = new JLabel("Game Mode");
+		lblGameMode.setBounds(220, 10, 76, 26);
+		contentPane.add(lblGameMode);
+		
+		String[] options = {"Select...", "Player vs Player", "Player vs PC", "PC vs PC"};
+		gameModeSelector = new JComboBox(options);
+		gameModeSelector.setBounds(295, 10, 130, 25);
+		contentPane.add(gameModeSelector);
+	}
+	
+	public void createNewGameButton() {
 		newGameButton = new JButton("Start new game");
 		newGameButton.setSize(130, 26);
 		newGameButton.setLocation(440, 10);
 		newGameButton.setPreferredSize(new Dimension(116, 40));
 		contentPane.add(newGameButton);
-		
-		createAboutButton();
-		
 	}
 	
 	public void createAboutButton() {
@@ -71,23 +94,26 @@ public class NineMensMorrisGUI extends JFrame {
 		aboutButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				JDialog d = new JDialog(aux, "About");
+				//JFrame principal
+				JFrame p = (JFrame)aboutButton.getParent().getParent().getParent().getParent();
 				
-				d.setLayout(new FlowLayout(FlowLayout.CENTER, 20, 20));
+				JDialog d = new JDialog(p, "About");
 				
-				d.add(new JLabel("Bernardo Manuel Costa Barbosa - up201503477")); 
+				d.getContentPane().setLayout(new FlowLayout(FlowLayout.CENTER, 20, 20));
+				
+				d.getContentPane().add(new JLabel("Bernardo Manuel Costa Barbosa - up201503477")); 
 				  
-				d.add(new JLabel("Duarte Nuno Andre Carvalho - up201503661"));
+				d.getContentPane().add(new JLabel("Duarte Nuno Andre Carvalho - up201503661"));
 				
-				d.add(new JLabel("Joao Pedro Teixeira Pereira de Sa - up201506252"));
+				d.getContentPane().add(new JLabel("Joao Pedro Teixeira Pereira de Sa - up201506252"));
 				
-				d.add(new JLabel("Artificial Intelligence"));
+				d.getContentPane().add(new JLabel("Artificial Intelligence"));
 				
-				d.add(new JLabel("Master in Informatics and Computing Engineering"));
+				d.getContentPane().add(new JLabel("Master in Informatics and Computing Engineering"));
 				
-				d.add(new JLabel("Faculty of Engineering  --- University of Porto"));
+				d.getContentPane().add(new JLabel("Faculty of Engineering  --- University of Porto"));
 				
-				d.setLocation(aux.getLocation().x + 200, aux.getLocation().y + 200);
+				d.setLocation(p.getLocation().x + 200, p.getLocation().y + 200);
 				
 	            d.setSize(400, 270); 
 	  
@@ -95,6 +121,6 @@ public class NineMensMorrisGUI extends JFrame {
 			}
 		});
 		contentPane.add(aboutButton);
+		
 	}
-
 }
